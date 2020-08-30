802322bada1c50e3b682f7dad01b2d30fd30e5d8
Maciej Panocha <maciej.panocha@gmail.com>
2020-08-30 15:18:06 +0200

    color redesign and adding about section

---
+++ src/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlaceOfWorship, faMagic, faDumbbell } from '@fortawesome/free-solid-svg-icons'
+
+const About = () => (
+    <div className="about">
+        <h2 className="about__h2">O nas</h2>
+        <p className="about__paragraph--slogan">Garść najważniejszych informacji o serwerze.</p>
+        <div className="about__div">
+            <h2 className="about__h3"><FontAwesomeIcon icon={faPlaceOfWorship} /></h2>
+            <p className="about__paragraph">Stworzyliśmy niesamowite miejsce dla ludzi, szukających nowych doświadczeń i fajnie zbalansowanego gameplayu.</p>
+        </div>
+        <div className="about__div">
+            <h2 className="about__h3"><FontAwesomeIcon icon={faMagic} /></h2>
+            <p className="about__paragraph">Codziennie polepszamy jakości rozgrywki i konsekwentnie budujemy zgraną społeczność.</p>
+        </div>
+        <div className="about__div">
+            <h2 className="about__h3"><FontAwesomeIcon icon={faDumbbell} /></h2>
+            <p className="about__paragraph">Niełatwo tu zagrać. Jednak kiedy się już dostaniesz, nie będziesz chciał stąd odejść. Społeczność mamy świetną!</p>
+        </div>
+    </div>
+);
+
+export default About;
