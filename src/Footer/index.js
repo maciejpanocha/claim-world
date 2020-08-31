095fefe7130f111e525f26a64b87a15d0397e6c7
Maciej Panocha <maciej.panocha@gmail.com>
2020-08-31 12:55:43 +0200

    added back to top of the website button

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 const Footer = () => (
     <div className="footer">
+        <a href="#header" className="footer__link--button">Wróć na górę strony.</a>
         <p className="footer__paragraph">
             Projekt i wykonanie: Maciej Panocha, <a href="https://maciejpanocha.pl" className="footer__link" target="_blank" rel="norefferal noopener" title="Strona internetowa autora">www.maciejpanocha.pl</a>
         </p>
